c4abeae7d606272a5994ca741a881ca7e368eaec
1525113309 <1712520508@qq.com>
2019-03-12 18:56:38 +0800

    modify_up.bat

--- up.bat
+++ up.bat
@@ -1,6 +1,7 @@
 @rem  1. 首次下载已经存在的项目 git clone https://github.com/用户名/仓库名   
-@rem  2. 上传文件到一个新项目 ， 首先 在GitHub新建一个仓库  ，然后执行 1.  ，再把文件拷贝到 1.所创建的文件夹  ， up.bat 也要放进去，cd 切换那个路径下 执行 ./up.bat  就好了！！！
-
+@rem  2. 上传文件到一个新项目 ， 首先 在GitHub新建一个仓库【勾选 Initialize this repository with a README】 
+@rem   ，然后执行 1.  ，再把文件拷贝到 1.所创建的文件夹  ， up.bat 也要放进去，cd 切换那个路径下 执行 ./up.bat  就好了！！！
+@rem  
 if "%gitUser%"=="" (set gitUser=1525113309)
 if "%gitMail%"=="" (set gitMail=1712520508@qq.com)
 @rem 更换用户名 需要在 【凭据管理器】》【Windows】 中删除原来的
@@ -11,7 +12,12 @@ git pull origin master
 
 if not defined wspath (set wspath=G:\QGB\babun\cygwin\home\qgb\wshell\)
 if "%*"=="" (for /f "delims=" %%i in ('%wspath%datetime.bat') do set commit_msg=%%i ) else (set commit_msg=%*)
-if "%commit_msg%"=="" (set commit_msg=default up)
+
+for /F "delims=" %%i  in ('date /t') do (set sdate=%%i)
+for /f "delims=" %%i in ('time /t') do (set stime=%%i)
+set sdate=%sdate: =%
+set datetime=%sdate%_%stime%
+if "%commit_msg%"=="" (set commit_msg=%datetime%)
 
 
 git config --global user.email %gitMail%
